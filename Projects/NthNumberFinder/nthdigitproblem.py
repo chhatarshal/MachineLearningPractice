c0dc9d89a456517c53209e0ccfd659f3b5247d6a
Chhatarshal Singh <chauhan.natwal@hotmail.com>
2021-06-19 23:26:54 +0530

    added problem statement

--- Projects/NthNumberFinder/nthdigitproblem.py
+++ Projects/NthNumberFinder/nthdigitproblem.py
@@ -7,6 +7,15 @@ Original file is located at
     https://colab.research.google.com/drive/1H8goN27T8RoqlVdSuuFKwD-YPyfT0CII
 """
 
+
+"""
+Problem:  Given an integer n, return the nth digit of the infinite integer sequence [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ...].
+
+Input: n = 11
+Output: 0
+Explanation: The 11th digit of the sequence 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ... is a 0, which is part of the number 10.
+"""
+
 def findNthDigit(n):
   digit = 1
   digits_interval =9  
